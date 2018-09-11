0722bd075351f37f2396c7e82be7bc246d944e32
github username <github email address>
2018-09-11 11:33:32 +0100

    Added logo, correct svg format for images, basic css styling, event listeners to enlarge template

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const endPoint = 'http://localhost:3000/api/v1/templates'
 const templateContainer = document.querySelector('#template-header')
 
 
+
   function getTemplates() {
     return fetch(endPoint)
     .then(resp => resp.json())
@@ -17,14 +18,25 @@ const templateContainer = document.querySelector('#template-header')
     templateEl.innerHTML =`
         <h2>${template.name}</h2>
         <p>${template.level}</p>
-        <img src="${template.image_url}">
+        <div class="svg-container"></div>
     `
+    const container = templateEl.querySelector('.svg-container')
+    getSVG(template.image_url)
+      .then(svg => container.innerHTML = svg)
+
+    container.addEventListener('click', event => {
+      const imageContainer = document.querySelector('#image-container')
+      imageContainer.innerHTML = container.innerHTML
+    })
 
     templateContainer.appendChild(templateEl)
   }
 
 
-
+  function getSVG(url) {
+    return fetch(url)
+      .then(resp => resp.text())
+  }
 
 
 })
